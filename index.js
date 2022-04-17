9d917f254c2007f37d3e1322100c8d6abab5a46d
Arul <aruljothy.s@fleximc.com>
2022-04-17 09:27:58 +0530

    morgan import

--- index.js
+++ index.js
@@ -1,43 +1,59 @@
-
 const express = require("express");
 const app = express();
 const port = 2552;
-var bodyParser = require('body-parser')
+var bodyParser = require("body-parser");
 
-var { redisclient } = require('./libs/redis');
-const redisroute = require('./routes/redisroute');
-const chalk = require('chalk');
+const chalk = require("chalk");
 
-const expressSwagger = require('express-swagger-generator')(app);
+const rt = require("file-stream-rotator")
 
-var cors = require('cors')
-app.use(cors())
+var morgan = require("morgan");
+
+
+
+var { redisclient } = require("./libs/redis");
+const redisroute = require("./routes/redisroute");
+const expressSwagger = require("express-swagger-generator")(app);
+
+var cors = require("cors");
+// app.use(morgan("combined"));
+
+//Create a new named format
+morgan.token("timed", "A new :method request for :url was received. " +
+    "It took :total-time[2] milliseconds to be resolved")
+
+let writer = rt.getStream({ filename: "./logs/test.log", frequency: "daily", verbose: true });
+
+
+//use the new format by name
+app.use(morgan('combined', { stream: writer }))
+
+app.use(cors());
 app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept"
+    );
     res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
     next();
-})
+});
 
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use(bodyParser.urlencoded({ extended: false }))
-
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
 let options = {
     swaggerDefinition: {
         info: {
-            description: 'Nodejs Redis API',
-            title: 'NODE REDIS',
-            version: '1.0.0',
+            description: "Nodejs Redis API",
+            title: "NODE REDIS",
+            version: "1.0.0",
         },
-        host: 'localhost:2552',
+        host: "localhost:2552",
         // basePath: '/v1',
-        produces: [
-            "application/json",
-            "application/xml"
-        ],
-        schemes: ['http', 'https'],
+        produces: ["application/json", "application/xml"],
+        schemes: ["http", "https"],
         // securityDefinitions: {
         //     JWT: {
         //         type: 'apiKey',
@@ -48,14 +64,17 @@ let options = {
         // }
     },
     basedir: __dirname, //app absolute path
-    files: ['./routes/**/*.js'] //Path to the API handle folder
+    files: ["./routes/**/*.js"], //Path to the API handle folder
 };
-expressSwagger(options)
+expressSwagger(options);
 
 // Route Definition
-app.use('', redisroute);
+app.use("", redisroute);
 app.listen(port, () => {
     console.log(`Redis app listening on port ${port}`);
-    console.log(chalk.redBright.bgWhite.bold('use the URL to explore :  http://localhost:2552/api-docs'));
-    // log(chalk.blue.bgRed.bold('Hello world!'));
+    console.log(
+        chalk.redBright.bgWhite.bold(
+            "use the URL to explore :  http://localhost:2552/api-docs"
+        )
+    );
 });
